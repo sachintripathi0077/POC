dc8c18d64880dda2ada8d7c5c247e68c61960a3e
Sachin_Tripathi <sachintripathi0077@gmail.com>
2021-11-24 10:14:56 +0530

    Preview Header Changed

--- src/Components/PreviewHeader.js
+++ src/Components/PreviewHeader.js
@@ -1,38 +1,43 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class PreviewHeader extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-             profileImg:props.headerContent.profileImg,
-             title:props.headerContent.title
-        }
-    }
-    componentDidMount() {
-        console.log(this.props.headerContent)
-    }
-    // headerContent = state.headerContent;
-    render() {
-        return (
-            <div>
-                <div className="headerContainer">
-                    <div className="imageContainer">
-                        <div className="img-holder">
-                            <img src={this.state.profileImg} alt="Add Image" id="img" className="img" />
-                        </div>
-                    </div>
-                    <div className="titleContainer">
-                        <h1>{this.state.title}</h1>
-                    </div>
-                </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+      profileImg: props.headerContent ? props.headerContent.profileImg : "",
+      title: props.headerContent ? props.headerContent.title : "",
+    };
+  }
+  componentDidMount() {
+    console.log(this.props.headerContent);
+  }
+  // headerContent = state.headerContent;
+  render() {
+    return (
+      <div>
+        <div className="headerContainer">
+          <div className="imageContainer">
+            <div className="img-holder">
+              <img
+                src={this.state.profileImg}
+                alt="Add Image"
+                id="img"
+                className="img"
+              />
             </div>
-        )
-    }
+          </div>
+          <div className="titleContainer">
+            <h1>{this.state.title}</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state) => {
-    return {
-        headerContent: state.headerContent
-    }
-}
-export default connect(mapStateToProps)(PreviewHeader)
+  return {
+    headerContent: state.headerContent,
+  };
+};
+export default connect(mapStateToProps)(PreviewHeader);
